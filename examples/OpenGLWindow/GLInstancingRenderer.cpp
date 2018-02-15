94a3b2ea5783215f2abff4b3b97f7e86797585d1
yunfeibai <byf1658@gmail.com>
2018-02-15 14:31:34 -0800

    Merge remote-tracking branch 'upstream/master'

--- examples/OpenGLWindow/GLInstancingRenderer.cpp
+++ examples/OpenGLWindow/GLInstancingRenderer.cpp
@@ -2008,6 +2008,7 @@ void GLInstancingRenderer::drawLine(const float from[4], const float to[4], cons
 B3_ATTRIBUTE_ALIGNED16(struct) SortableTransparentInstance
 {
 	b3Scalar m_projection;
+
 	int m_shapeIndex;
 	int m_instanceId;
 };
@@ -2015,7 +2016,11 @@ B3_ATTRIBUTE_ALIGNED16(struct) SortableTransparentInstance
 B3_ATTRIBUTE_ALIGNED16(struct) TransparentDistanceSortPredicate
 {
 
+<<<<<<< HEAD
 	inline bool operator() (const SortableTransparentInstance& a, const SortableTransparentInstance& b) const
+=======
+	inline bool operator() (const SortableTransparentInstance& a, const SortableTransparentInstance& b) const 
+>>>>>>> Merge remote-tracking branch 'upstream/master'
 	{
 
 		return (a.m_projection > b.m_projection);
@@ -2257,8 +2262,8 @@ b3Assert(glGetError() ==GL_NO_ERROR);
 					inst.m_instanceId = curOffset;
 					b3Vector3 centerPosition;
 					centerPosition.setValue(m_data->m_instance_positions_ptr[inst.m_instanceId*4+0],
-											m_data->m_instance_positions_ptr[inst.m_instanceId*4+1],
-											m_data->m_instance_positions_ptr[inst.m_instanceId*4+2]);
+								m_data->m_instance_positions_ptr[inst.m_instanceId*4+1],
+								m_data->m_instance_positions_ptr[inst.m_instanceId*4+2]);
 					centerPosition *= -1;//reverse sort opaque instances
 					inst.m_projection = centerPosition.dot(camForwardVec);
 					transparentInstances.push_back(inst);
@@ -2268,10 +2273,10 @@ b3Assert(glGetError() ==GL_NO_ERROR);
 					{
 						inst.m_instanceId = curOffset+i;
 						b3Vector3 centerPosition;
-						
+
 						centerPosition.setValue(m_data->m_instance_positions_ptr[inst.m_instanceId*4+0],
-												m_data->m_instance_positions_ptr[inst.m_instanceId*4+1],
-												m_data->m_instance_positions_ptr[inst.m_instanceId*4+2]);
+										m_data->m_instance_positions_ptr[inst.m_instanceId*4+1],
+										m_data->m_instance_positions_ptr[inst.m_instanceId*4+2]);
 						inst.m_projection = centerPosition.dot(camForwardVec);
 						transparentInstances.push_back(inst);
 					}
